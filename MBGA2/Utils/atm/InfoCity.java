8044070aa2d7f632d31decbe75b85fb0d78db352
SergioJoseGC <sergiojosegc@gmail.com>
2020-04-30 22:56:37 +0200

    Bruh Path

---
+++ MBGA2/Utils/atm/InfoCity.java
@@ -0,0 +1,31 @@
+package Utils.atm;
+
+public class InfoCity {
+	private String name;
+	private int posX;
+	private int posY;
+	public InfoCity(String name, int posX, int posY) {
+		super();
+		this.name = name;
+		this.posX = posX;
+		this.posY = posY;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+}
