6939f19cb23b9bb7ede5833636c4257952afdf02
nachopaank <igndelac@ucm.es>
2020-05-16 21:34:59 +0200

    añadidas funcionalidades de mapa

--- MBGA2/DAOs/map_dao.java
+++ MBGA2/DAOs/map_dao.java
@@ -1,11 +1,57 @@
 package DAOs;
 
-import javax.swing.JTable;
+import java.sql.Timestamp;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import Datos.map.Mapm;
+import Datos.map.NumberFlights;
+import Vista.MainView;
 
 public class map_dao {
 	public map_dao() {
 	}
-	public void fill(JTable table) {
-		
+	public void fill(DefaultTableModel f, Mapm m, Timestamp timestamp) {
+		for (int i = 0; i < 14; i++) {
+			f.addRow(constructRow(i,m,timestamp));
+
+		}
+	}
+	public Object[] constructRow(int i, Mapm m, Timestamp timestamp) {
+		Object[] fila = new Object[20];
+		for (int j = 0; j < 20; j++) {
+			if(m.getInfoMap().containsKey(j+","+i)) {
+				if(hasPlanes(m.getInfoMap().get(j+","+i),timestamp)) {
+					String s;
+					if(m.getInfoMap().get(j+","+i).flightsOnTime(timestamp)>1) {
+						s = "/Iconos/goingMul.png";
+					}
+					else {
+						s = "/Iconos/going.png";
+					}
+					fila[j] = new ImageIcon( new ImageIcon(MainView.class.getResource(s)).getImage().getScaledInstance( 50, 50,  java.awt.Image.SCALE_SMOOTH ));	
+
+	            }
+				else {
+					fila[j] = null;
+				}
+			}
+			else {
+				fila[j] = null;
+			}
+		}
+		return fila;
+	}
+	private Boolean hasPlanes(NumberFlights value, Timestamp timestamp) {
+		if (value.flightsOnTime(timestamp) > 0) {
+		return true;
+		}
+		return false;
+	}
+	public void showPopUp(String s) {
+		JOptionPane.showConfirmDialog(null,
+                s, "", JOptionPane.DEFAULT_OPTION);
 	}
 }
--- MBGA2/SA/map_SA.java
+++ MBGA2/SA/map_SA.java
@@ -1,24 +1,57 @@
 package SA;
 
-import java.util.List;
 
-import javax.swing.JTable;
+import java.sql.Timestamp;
+
 import javax.swing.table.DefaultTableModel;
 
-import DAOs.inf_dao;
 import DAOs.map_dao;
 import Datos.Flight;
+import Datos.map.Mapm;
+import Datos.map.NumberFlights;
 
 public class map_SA {
 	private map_dao dao;
-	private JTable f;
+	private DefaultTableModel f;
 	public map_SA() {
 		this.dao = new map_dao();
 	}
-	public void fill() {
-		this.dao.fill(f);
+	public void fill(Mapm m, Timestamp timestamp) {
+		f.setRowCount(0);
+		this.dao.fill(f, m,timestamp);
+		f.fireTableDataChanged();
 	}
-	public void setF(JTable table) {
-		this.f = table;
+	public void setF(DefaultTableModel tableModel) {
+		this.f = tableModel;
+	}
+	public void infoC(int col, int row, Mapm m, Timestamp ini) {
+		String s = "";
+		NumberFlights n = m.getInfoMap().get(row+","+col);
+		for (int i = 0; i < n.getInfoFlights().size(); i++) {
+			if (n.getInfoFlights().get(i).getInitialD().before(ini) && n.getInfoFlights().get(i).getLastD().after(ini)) {
+			Flight f = n.getInfoFlights().get(i).getFlight();
+			s = s+ "Origen: "+ f.getSource() + "(" + parseDate(f.getDeparture_time()) + ")\n"+"Destino: "+ f.getDestination() + "(" + parseDate(f.getArrival_time()) + ")\n"+"Estado: "+f.getPlane_state()+"\n"+"--------------"+"\n";
+			}
+		}
+		if (s!="") {
+		this.dao.showPopUp(s);
+		}
+		
+	}
+	@SuppressWarnings("deprecation")
+	private String parseDate(Timestamp date) {
+		if (date == null) {
+			return "";
+		}
+		String h = Integer.toString(date.getHours());
+		String m = Integer.toString(date.getMinutes());
+		if (date.getHours() < 10) {
+			h = "0" + h;
+		}
+		if (date.getMinutes() < 10) {
+			m = "0" + m;
+		}
+
+		return h + ":" + m;
 	}
 }
