1d094af2ecdc402c9483294c5f3e05e91edda32f
nachopaank <igndelac@ucm.es>
2020-05-18 20:26:34 +0200

    ahora relleno el mapa aqui

--- MBGA2/Controladores/map_controller.java
+++ MBGA2/Controladores/map_controller.java
@@ -1,7 +1,11 @@
 package Controladores;
 
+import java.sql.Timestamp;
+
 import javax.swing.table.DefaultTableModel;
 
+import Datos.Flight;
+import Datos.InfoCity;
 import Main.Aeropuerto;
 import Observer.Observer;
 import SA.map_SA;
@@ -31,6 +35,85 @@ public class map_controller {
 		for (int i = 0; i < this.aero.getFligths().size(); i++) {
 			aero.getFligths().get(i).addObserver(o);
 		}
+	}
+	public void fillMap(Flight f) {
+		InfoCity destInfo = null, sourceInfo = null, stopInfo = null;
+		int j = 0, maxFlightsBox = 20;
+
+		// Busco el valor de las ciudades origen, destino y escala
+		for (InfoCity aux : this.aero.getPath().getCities())
+			if (aux.getName().equalsIgnoreCase(f.getDestination()))
+				destInfo = aux;
+			else if (aux.getName().equalsIgnoreCase(f.getSource()))
+				sourceInfo = aux;
+			else if (aux.getName().equalsIgnoreCase(f.getPath().getStopover()))
+				stopInfo = aux;
+
+		int xSource = sourceInfo.getPosX(), ySource = sourceInfo.getPosY(), xDest = destInfo.getPosX(),
+				yDest = destInfo.getPosY(), ti = 30 * 60 * 1000;
+		Timestamp t = f.getDeparture_time();
+
+		if (stopInfo != null) {
+			int xStop = stopInfo.getPosX(), yStop = stopInfo.getPosY();
+
+			while (xSource != xStop && ySource != yStop) {
+				if (xSource < xStop && this.aero.getMap().checkFlightsInMap(xSource + 1, ySource,
+						new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+					this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+							new Timestamp(t.getTime() + (ti * (j + 1))));
+					xSource++;
+					j++;
+				} else if (xSource > xStop && this.aero.getMap().checkFlightsInMap(xSource - 1, ySource,
+						new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+					this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+							new Timestamp(t.getTime() + (ti * (j + 1))));
+					xSource--;
+					j++;
+				}
+				if (ySource < yStop && this.aero.getMap().checkFlightsInMap(xSource, ySource + 1,
+						new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+					this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+							new Timestamp(t.getTime() + (ti * (j + 1))));
+					ySource++;
+				} else if (ySource > yStop && this.aero.getMap().checkFlightsInMap(xSource, ySource - 1,
+						new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+					this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+							new Timestamp(t.getTime() + (ti * (j + 1))));
+					ySource--;
+				}
+				j++;
+			}
+			// Recorro de origen a escala y de escala a destino si hay escala
+
+		}
+		while (xSource != xDest || ySource != yDest) {
+			if (xSource < xDest && this.aero.getMap().checkFlightsInMap(xSource + 1, ySource,
+					new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+				this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+						new Timestamp(t.getTime() + (ti * (j + 1))));
+				xSource++;
+				j++;
+			} else if (xSource > xDest && this.aero.getMap().checkFlightsInMap(xSource - 1, ySource,
+					new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+				this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+						new Timestamp(t.getTime() + (ti * (j + 1))));
+				xSource--;
+				j++;
+			}
+			if (ySource < yDest && this.aero.getMap().checkFlightsInMap(xSource, ySource + 1,
+					new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+				this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+						new Timestamp(t.getTime() + (ti * (j + 1))));
+				ySource++;
+			} else if (ySource > yDest && this.aero.getMap().checkFlightsInMap(xSource, ySource - 1,
+					new Timestamp(t.getTime() + (ti * j))) < maxFlightsBox) {
+				this.aero.getMap().addFlightInMap(f, xSource, ySource, new Timestamp(t.getTime() + (ti * j)),
+						new Timestamp(t.getTime() + (ti * (j + 1))));
+				ySource--;
+			}
+			j++;
+		}
+
 	}
 	public void refresh() {
 		this.SA.fill(this.aero.getMap(),this.aero.getTime());
